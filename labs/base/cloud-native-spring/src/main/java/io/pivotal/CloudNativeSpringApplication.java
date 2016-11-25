452c83efe11ee930e69fa6654626b5fbac304dbd
phopper-pivotal <phopper@pivotal.io>
2016-11-24 23:12:09 -0600

    rebase diretory of Spring Boot 1.3.8

--- labs/base/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
+++ labs/base/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -1,5 +1,6 @@
 package io.pivotal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -14,12 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Import(RepositoryRestMvcConfiguration.class) // <---- And this
 public class CloudNativeSpringApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
     @RequestMapping("/")
     public String hello() {
         return "Hello World!";
     }
-
-	public static void main(String[] args) {
-		SpringApplication.run(CloudNativeSpringApplication.class, args);
-	}
 }
